4bc814c4e8cc2b17963da685794e8ed3a537d17f
Sebastian Schubert <sebastian.schubert@tngtech.com>
2024-11-15 14:04:18 +0100

    Added personal information

--- src/main.js
+++ src/main.js
@@ -4,26 +4,23 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'Christof Happ',
-		// In the following fiels you can either give a single string, 
+		name: 'Sebastian Schubert',
+		// In the following fields you can either give a single string,
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Red tiles on a screen on the wall',
-			'Scripts in strange programming languages',
-			'Some annoying technology preventing me to finish my ticket and breaking production'
+			'Pipelines', 'Automatisation'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: ':sadpanda:',
+		tools: ':unsure:',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'Share knowledge about the basics of CI/CD'
+			'Newbie introduction to CI/CD'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'reading books', 
-			'cooking', 
+			'family',
 			'playing computer games'
 		]
 	}
