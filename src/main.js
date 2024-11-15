d72c8ea00101dfed832d278d956eb918dea2175b
cjhappTNG <94829904+cjhappTNG@users.noreply.github.com>
2024-11-15 10:35:54 +0100

    Introducing myself again

--- src/main.js
+++ src/main.js
@@ -4,25 +4,27 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Christof Happ',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'Red tiles on a screen on the wall',
+			'Scripts in strange programming languages',
+			'Some annoying technology preventing me to finish my ticket and breaking production'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: ':sadpanda:',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'Share knowledge about the basics of CI/CD'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'reading books', 
+			'cooking', 
+			'playing computer games'
 		]
 	}
 });
